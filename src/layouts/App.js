c8c487bd88ac49d10ab635ff027980cc5846bf7e
Juanchi36 <juanmartingon@hotmail.com>
2020-07-19 11:23:36 -0300

    Starting project

---
+++ src/layouts/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../views/Home/Home';
+import AppBar from '../components/AppBar/AppBar'
+
+function App() {
+  return (
+    <div>
+      <AppBar />
+      <Home />
+    </div>
+  )
+}
+
+export default App;
